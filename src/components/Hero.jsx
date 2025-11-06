955110ea5e7e54e69f8d4479c39b3b81c5ca7320
edsonmendz <japaact@gmail.com>
2025-11-05 23:37:00 -0300

    hero corrigido web

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,24 +16,27 @@ export default function Hero() {
               <a className="btn ghost" href="#profile">Conheça o Sensei</a>
             </div>
           </div>
-
           <div className="hero-cards">
-            <article className="card card-1">
-              <img src="https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/adfcf9b1253df6b137ad173210ee2852.jpg" alt="Aula 1" />
-              <div className="card-overlay"></div>
-              <div className="card-text">Aulas de Judô Tradicional</div>
-            </article>
-            <article className="card card-2">
-              <img src="https://as1.ftcdn.net/jpg/05/48/64/48/1000_F_548644845_n732ZQyO87mHAb5XyOxYg8yaGwRWddz7.jpg" alt="Aula 2" />
-              <div className="card-overlay"></div>
-              <div className="card-text">Defesa Pessoal</div>
-            </article>
-            <article className="card card-3">
-              <img src={judoInclusivoImg} alt="Judô inclusivo" />
-              <div className="card-overlay"></div>
-              <div className="card-text">Judô inclusivo</div>
-            </article>
-          </div>
+            <div className="hero-card-horizontal">
+              <article className="card card-1">
+                  <img src="https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/adfcf9b1253df6b137ad173210ee2852.jpg" alt="Aula 1" />
+                  <div className="card-overlay"></div>
+                  <div className="card-text">Aulas de Judô Tradicional</div>
+                </article>
+                <article className="card card-2">
+                  <img src="https://as1.ftcdn.net/jpg/05/48/64/48/1000_F_548644845_n732ZQyO87mHAb5XyOxYg8yaGwRWddz7.jpg" alt="Aula 2" />
+                  <div className="card-overlay"></div>
+                  <div className="card-text">Defesa Pessoal</div>
+                </article>
+            </div>
+            <div className="hero-card-vertical">
+              <article className="card card-3">
+                <img src={judoInclusivoImg} alt="Judô inclusivo" />
+                <div className="card-overlay"></div>
+                <div className="card-text">Judô inclusivo</div>
+              </article>
+            </div>
+          </div>   
         </div>
       </div>
     </div>
